b31afd96bcd426c3188132d99b2087f215f41fa3
Paul <pstanfel@alumni.mines.edu>
2021-07-19 06:37:33 -0600

    Modified functions for data import and wind direction plotting.

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import tensorflow.keras as keras
 import numpy as np
-from data import import_data
+from data import import_data, manipulate_data
 
 # load saved model
 model = keras.models.load_model(".\\wind_direction_model.h5")
@@ -26,3 +26,24 @@ row_1_path = data_path + '1efc54797915c2931df2e161f8e6ed14\\'
 row_2_path = data_path + '288afbdd498141c7f270a74f8c455605\\'
 
 row_data = import_data(row_0_path, row_1_path, row_2_path)
+
+########################### Data Manipulation ###########################
+
+# indices 5,6, and 8 correspond to wind direction, wind speed, and pressure, respectively
+ws_index = 5
+wd_index = 6
+pr_index = 8
+indices = [ws_index, wd_index, pr_index]
+
+# define time horizon in minutes
+horizon = 30
+
+# define time step in minutes
+time_step = 5
+
+X, y = manipulate_data(row_data, wd_index, indices, horizon, time_step)
+
+X_test = X
+y_test = y
+
+print(model.evaluate(X_test, y_test))
--- train.py
+++ train.py
@@ -24,6 +24,14 @@ row_2_path = data_path + '0fcb7c7e955e55af9376820c9ba9824f\\'
 
 row_data = import_data(row_0_path, row_1_path, row_2_path)
 
+# code for plotting wind direction at site (1,1)
+# plt.rcParams.update({'font.size': 22})
+# plt.plot(row_data[1][1][:,6])
+# plt.xlabel("Time")
+# plt.ylabel("Wind Direction (deg.)")
+# plt.title("Wind Direction vs. Time, Site (1,1)")
+# plt.show()
+
 ########################### Data Manipulation ###########################
 # indices 5,6, and 8 correspond to wind direction, wind speed, and pressure, respectively
 ws_index = 5
@@ -32,14 +40,18 @@ pr_index = 8
 indices = [ws_index, wd_index, pr_index]
 
 # define time horizon in minutes
-horizon = 30
+horizon = 10
 
 # define time step in minutes
 time_step = 5
 
 X, y = manipulate_data(row_data, wd_index, indices, horizon, time_step)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+# if testing dataset is not available, train_test_split can be used to generate one
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+X_train = X
+y_train = y
 
 ########################### Model Training ###########################
 # create neural network
@@ -53,4 +65,5 @@ model = keras.Sequential(layers)
 model.compile(optimizer='Adam', loss='mae', metrics=['mean_absolute_error', 'mean_squared_error'] )
 model.fit(X_train, y_train, epochs=20)
 
+# save model
 #model.save(".\\wind_direction_model.h5")
